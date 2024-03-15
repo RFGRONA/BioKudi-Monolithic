d6609594944041f9fbd7225aed1be8156adf85ad
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-03-15 07:37:46 -0500

    Example files added

---
+++ Repository/dbRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BioKudi.Repository
+{
+    public class dbRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ dto/dto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BioKudi.dto
+{
+    public class dto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

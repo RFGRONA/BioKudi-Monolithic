c62a924097101ece1f87ad3b63382cb7071afb74
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-03-28 17:54:59 -0500

    View's Added
    Vistas User[Register y Login] añadidas
    Añadido Css de cada vista
    Conexiones a través de controladores añadidas

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BioKudi.Models;
+using System.Diagnostics;
+namespace BioKudi.Controllers
+{
+	public class UserController : Controller
+	{
+		private readonly ILogger<UserController> _logger;
+
+
+		public UserController(ILogger<UserController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}

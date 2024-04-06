b63a778738c3230aa2244afb3a00fb20fc25f4df
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-04-05 19:23:53 -0500

    AppSettings Conection + FrontEnd
    Correción de arranque de aplicación
     Ajustes en css de vistas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace BioKudi.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

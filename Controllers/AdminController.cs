3445d02277de033cb3dcc80230345811137292b5
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-04-16 22:09:49 -0500

    Injection Dependency and URL Fix
    Se hizo inyección de dependencias de UserService para poder solucionar el problema de mostrar toda la información del usuario en el URL

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,21 +1,27 @@
-﻿using BioKudi.Models;
+﻿using BioKudi.dto;
+using BioKudi.Models;
+using BioKudi.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BioKudi.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         private readonly ILogger<AdminController> _logger;
+        private readonly UserService userService;
 
-        public AdminController(ILogger<AdminController> logger)
+        public AdminController(ILogger<AdminController> logger, UserService userService)
         {
             _logger = logger;
+            this.userService = userService;
         }
 
-        public IActionResult IndexAdmin()
+        public IActionResult IndexAdmin(UserDto user)
         {
-            return View();
+            user = userService.GetUser(user.UserId);
+            return View(user);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

20e92ad62075a3125e2723ee7c5fd4b65b8959de
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-04-26 14:08:22 -0500

    RF, attributes added
    Se añadieron los atributos de verificación para el inicio de sesión y manejo de vista según rol. También se añadió el atributo para evitar que los sitios se almacenen en caché

---
+++ Utilities/ValidateAuthenticationAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BioKudi.Utilities
+{
+	// Attribute that checks if the user is logged in, if not redirects them to home
+	public class ValidateAuthenticationAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			if (!context.HttpContext.User.Identity.IsAuthenticated)
+			{
+				context.Result = new RedirectToActionResult("Index", "Home", null);
+			}
+
+			base.OnActionExecuting(context);
+		}
+	}
+}

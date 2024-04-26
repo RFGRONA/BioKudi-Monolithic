e90743e493b0e171d401815459c95fe522a9bfd7
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-04-26 13:58:31 -0500

    RF, dtos added
    Se agregaron los dtos restantes y referentes al modelo

---
+++ dto/ActivityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BioKudi.dto
+{
+    public class ActivityDto
+    {
+        public int IdActivity { get; set; }
+        public string Type { get; set; } = null!;
+        public List<int> PlaceId { get; set; } = new List<int>();
+    }
+}
---
+++ dto/RoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BioKudi.dto
+{
+    public class RoleDto
+    {
+        public int RoleId { get; set; }
+        public string? NameRole { get; set; }
+    }
+}

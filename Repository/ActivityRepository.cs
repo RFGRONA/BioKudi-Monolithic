f3a51ccb18a7a252b18072787b424a973c1dcd79
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-04-26 13:59:24 -0500

    RF, repositories added
    Se agregaron los repositorios restantes, referentes al modelo y a los dtos

---
+++ Repository/ActivityRepository.cs
@@ -0,0 +1,49 @@
+﻿using BioKudi.dto;
+using BioKudi.Models;
+
+namespace BioKudi.Repository
+{
+    public class ActivityRepository
+	{
+		private readonly BiokudiDbContext _context;
+
+		public ActivityRepository(BiokudiDbContext context)
+		{
+			_context = context;
+		}
+
+		public ActivityDto Create(ActivityDto activity)
+		{
+			var newActivity = new Activity
+			{
+				Type = activity.Type
+			};
+			_context.Activities.Add(newActivity);
+			_context.SaveChanges();
+			return activity;
+		}
+
+		public IEnumerable<ActivityDto> GetListActivity()
+		{
+			return _context.Activities.Select(a => new ActivityDto
+			{
+				IdActivity = a.IdActivity,
+				Type = a.Type
+			}).ToList();
+		}
+
+		public ActivityDto GetActivity(int activityId)
+		{
+			var activity = _context.Activities.FirstOrDefault(a => a.IdActivity == activityId);
+			if (activity == null)
+			{
+				return null;
+			}
+			return new ActivityDto
+			{
+				IdActivity = activity.IdActivity,
+				Type = activity.Type
+			};
+		}
+	}
+}

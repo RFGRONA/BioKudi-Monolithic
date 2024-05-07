508d50b8a228911012135ec992d0ee42ecbc96e8
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-05-07 16:50:10 -0500

    RF, GetRoles function added

---
+++ Services/RoleService.cs
@@ -0,0 +1,22 @@
+﻿using BioKudi.dto;
+using BioKudi.Repository;
+
+namespace BioKudi.Services
+{
+    public class RoleService
+    {
+        private readonly RoleRepository roleRepository;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public RoleService(RoleRepository roleRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            this.roleRepository = roleRepository;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public IEnumerable<RoleDto> GetRoles()
+        {
+            return roleRepository.GetRoles();
+        }
+    }
+}

4232abdbff9f0f9da1f4d0d7297fb2bb7e1e8f37
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-05-15 10:32:08 -0500

    CF, String connection

--- Models/BiokudiDbContext.cs
+++ Models/BiokudiDbContext.cs
@@ -34,17 +34,17 @@ public partial class BiokudiDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    //Data base connection string
-    //public static string ConnectionString = "Data Source=biokudi-server.database.windows.net,1433;Initial Catalog=biokudi-database;User ID=biokudi-server-admin;Password=8Zc3nq1B$xAUlb4R";
-    public static string ConnectionString = "server=localhost; database=BIOKUDI-DB; user id=sysbiokudi; password=BK2a2; TrustServerCertificate=Yes;";
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-            optionsBuilder.UseSqlServer(ConnectionString);
-        base.OnConfiguring(optionsBuilder);
-    }
+	//Data base connection string
+	//public static string ConnectionString = "Data Source=biokudi-server.database.windows.net,1433;Initial Catalog=biokudi-database;User ID=biokudi-server-admin;Password=8Zc3nq1B$xAUlb4R";
+	public static string ConnectionString = "server=FREDY\\MSSQLSERVER01; database=BIOKUDI-DB; integrated security=true; TrustServerCertificate=Yes;";
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		if (!optionsBuilder.IsConfigured)
+			optionsBuilder.UseSqlServer(ConnectionString);
+		base.OnConfiguring(optionsBuilder);
+	}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Activity>(entity =>
         {

83a755ebec15f815d77921f342f560f97cdeea08
Fredy Zarate <fazarate@ucundinamarca.edu.co>
2024-05-16 17:56:44 -0500

    FX, review views

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -28,73 +28,15 @@ namespace BioKudi.Controllers
             return View(reviews);
         }
 
-        // GET: ReviewController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ReviewController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ReviewController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ReviewController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ReviewController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ReviewController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
         // POST: ReviewController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id)
         {
-            try
-            {
+                var review = reviewService.DeleteReview(id);
+                if (!review)
+                    RedirectToAction("Error", "Admin");
                 return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
